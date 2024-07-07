09a32f71a19e384fb5d2d43deddb983288788c71
Sonja Komlen <komlensonja@gmail.com>
2024-07-07 14:02:09 +0200

    submit

---
+++ reactsajt/vite-project/src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "./ContactForm.css";
+
+const ContactForm: React.FC = () => {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Ova funkcija bi trebala poslati obrazac s podacima:", {
+      name,
+      email,
+      message,
+    });
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+
+};
+
+export default ContactForm;
