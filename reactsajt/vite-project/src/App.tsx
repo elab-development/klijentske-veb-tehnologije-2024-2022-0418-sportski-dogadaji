32fb9d3ed8bc56c82d0b11899583299ccd1f774e
Sonja Komlen <komlensonja@gmail.com>
2024-07-08 12:34:33 +0200

    app

--- reactsajt/vite-project/src/App.tsx
+++ reactsajt/vite-project/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Contact from "./pages/Contact/Contact";
+import NavBar from "./components/NavBar/NavBar";
+import Kosarka from "./pages/Kosarka/Kosarka";
+import Fudbal from "./pages/Fudbal/Fudbal";
+import Kreiraj from "./pages/Kreiraj/Kreiraj";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/kosarka" element={<Kosarka />} />
+        <Route path="/fudbal" element={<Fudbal />} />
+        <Route path="/kreiraj" element={<Kreiraj />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
