7408519530fe8c3eea044dacdae54cb3bd4b3629
Sonja Komlen <komlensonja@gmail.com>
2024-07-08 13:15:30 +0200

    app

--- reactsajt/vite-project/src/App.tsx
+++ reactsajt/vite-project/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./pages/About/About";
+//import Home from "./pages/Home/Home";
 import Home from "./pages/Home/Home";
 import Products from "./pages/Products/Products";
 import Contact from "./pages/Contact/Contact";
