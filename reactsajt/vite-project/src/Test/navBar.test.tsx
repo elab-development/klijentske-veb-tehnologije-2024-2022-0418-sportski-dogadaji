5256a2d08ee941e2f1403de964028dfa46a97d6e
Sonja Komlen <komlensonja@gmail.com>
2024-07-08 16:41:49 +0200

    navbar-test

---
+++ reactsajt/vite-project/src/Test/navBar.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "@testing-library/jest-dom"
+import NavBar from "../components/NavBar/NavBar";
+
+describe("NavBar", () => {
+  test("renders NavBar component with correct links", () => {
+    render(
+      <Router>
+        <NavBar />
+      </Router>
+    );
+
+    expect(screen.getByText("Početna")).toBeInTheDocument();
+    expect(screen.getByText("O nama")).toBeInTheDocument();
+    expect(screen.getByText("Turniri")).toBeInTheDocument();
+    expect(screen.getByText("Kontakt")).toBeInTheDocument();
+  });
+});
